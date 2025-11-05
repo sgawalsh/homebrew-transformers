ac52e31f681a338f856830b55d1c1440b7db0026
Stephen Walsh <sgawalsh@gmail.com>
2025-11-05 18:40:52 -0500

    Add shutdown flag, update parameters

--- settings.py
+++ settings.py
@@ -3,11 +3,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 MAX_LEN = 2000 # Maximum tokens per sentence, can cause OOM error if too high so set accordingly
 MIN_LEN = 1 # Minimum tokens per sentence
-MODEL_PARAMS = "Full" # Determines model parameters from modelDict in settings.py
-MAX_TOKENS = 7500 # Set lower for larger models or less processing power
+MODEL_PARAMS = "Small" # Determines model parameters from modelDict in settings.py
+MAX_TOKENS = 2000 # Set lower for larger models or less processing power
 SRC_LANG = "en" # Source language
 TRG_LANG = "de" # Target language
 DATA_MODE = "wmt" # europarl or wmt source data
+SHUTDOWN = False # Shutdown PC after training completes
 
 dataFileDict = {
     "europarl": {
