132aba4a50b62589f519be6b5463db0c0f6c8f79
Stephen Walsh <sgawalsh@gmail.com>
2025-11-12 14:25:27 -0500

    Update settings for docker

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-docker build -t homebrew-transformers .
-docker run --rm homebrew-transformers
+# Build the image
+docker build -t transformers .
+
+# Run the container with mounted directories
+docker run \
+    -v "${PWD}/checkpoints:/checkpoints" \
+    -v "${PWD}/data:/data" \
+    transformers
