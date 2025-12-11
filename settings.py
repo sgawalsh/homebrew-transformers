de5ab9655d7b7cf08023b97b684bbdca415e0fbb
Stephen Walsh <sgawalsh@gmail.com>
2025-12-11 09:32:45 -0500

    New suggested settings

--- settings.py
+++ settings.py
@@ -3,12 +3,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 MAX_LEN = 2000 # Maximum tokens per sentence, can cause OOM error if too high so set accordingly
 MIN_LEN = 1 # Minimum tokens per sentence
-MODEL_PARAMS = "Small" # Determines model parameters from modelDict in settings.py
+MODEL_PARAMS = "Full" # Determines model parameters from modelDict in settings.py
 MAX_TOKENS = 2000 # Set lower for larger models or less processing power
 MAX_TOKENS_REF = 50000 # Number of tokens used in original transformer paper
 SRC_LANG = "en" # Source language
-TRG_LANG = "de" # Target language
-DATA_MODE = "wmt" # europarl or wmt source data
+TRG_LANG = "fr" # Target language
+DATA_MODE = "europarl" # europarl or wmt source data
 SHUTDOWN_ON_COMPLETE = False # Shutdown PC after training completes
 SHUTDOWN_ON_ERROR = False # Shutdown PC after error occurs
 
